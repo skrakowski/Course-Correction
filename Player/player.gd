cbd3b038b84866d43685264776d8e2d638b62e8a
rgipsonhansen <45026386+rgipsonhansen@users.noreply.github.com>
2023-11-17 13:56:03 -0700

    Movement prototype, camera changes, placeholder sprites

--- Player/player.gd
+++ Player/player.gd
@@ -1,28 +1,17 @@
 extends CharacterBody2D
 
+const ACC = 5
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _physics_process(_delta):
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# Get the input direction and handle the movement/acceleration.
+	var direction = Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	)
+	
+	velocity += direction * ACC
+	
+#	print(velocity)
+	
 	move_and_slide()
