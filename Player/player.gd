91acc5a58b39a0fffb6a831c495e189f72420590
skrakowski <bastiankraken@gmail.com>
2023-11-17 16:24:09 -0600

    Added bullet interaction. Still working on adding velocity

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,19 @@
 extends CharacterBody2D
 
 const ACC = 5
+const bulletPath = preload("res://Interactions/bullet.tscn")
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func shoot():
+	var bullet = bulletPath.instantiate()
+
+	get_parent().add_child(bullet)
+	bullet.position = $Marker2D.global_position
+
+
 
 func _physics_process(_delta):
 
@@ -12,6 +25,14 @@ func _physics_process(_delta):
 	
 	velocity += direction * ACC
 	
-#	print(velocity)
+
+
+	
+#func shoot():
+#	var bullet = bulletPath.instantiate()
+#
+#	get_parent().add_child(bullet)
+#	bullet.position = $Marker2D.global_position
+	
 	
 	move_and_slide()
