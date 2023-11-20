7a0f0785cbd39e4a92814312efdce7f44c774d8a
skrakowski <bastiankraken@gmail.com>
2023-11-20 17:42:37 -0600

    Working on getting mouse tracking functional. In progress

--- Player/player.gd
+++ Player/player.gd
@@ -11,12 +11,14 @@ func shoot():
 	var bullet = bulletPath.instantiate()
 
 	get_parent().add_child(bullet)
-	bullet.position = $Marker2D.global_position
-
+	bullet.position = $Node2D/Marker2D.global_position
+	
+	
+	#bullet.velocity = get_global_mouse_position() - bullet.position
 
 
 func _physics_process(_delta):
-
+	
 	# Get the input direction and handle the movement/acceleration.
 	var direction = Vector2(
 		Input.get_axis("ui_left", "ui_right"),
@@ -25,14 +27,8 @@ func _physics_process(_delta):
 	
 	velocity += direction * ACC
 	
+	$Node2D.look_at(get_global_mouse_position())
 
 
 	
-#func shoot():
-#	var bullet = bulletPath.instantiate()
-#
-#	get_parent().add_child(bullet)
-#	bullet.position = $Marker2D.global_position
-	
-	
-	move_and_slide()
+
