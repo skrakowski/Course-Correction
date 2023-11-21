714a1dce0e5dca2b477ed7164e1388e0988c0448
skrakowski <bastiankraken@gmail.com>
2023-11-20 18:23:54 -0600

    Fixed previous push error

--- Player/player.gd
+++ Player/player.gd
@@ -11,14 +11,12 @@ func shoot():
 	var bullet = bulletPath.instantiate()
 
 	get_parent().add_child(bullet)
-	bullet.position = $Node2D/Marker2D.global_position
-	
-	
-	#bullet.velocity = get_global_mouse_position() - bullet.position
+	bullet.position = $Marker2D.global_position
+
 
 
 func _physics_process(_delta):
-	
+
 	# Get the input direction and handle the movement/acceleration.
 	var direction = Vector2(
 		Input.get_axis("ui_left", "ui_right"),
@@ -27,8 +25,14 @@ func _physics_process(_delta):
 	
 	velocity += direction * ACC
 	
-	$Node2D.look_at(get_global_mouse_position())
 
 
 	
-
+#func shoot():
+#	var bullet = bulletPath.instantiate()
+#
+#	get_parent().add_child(bullet)
+#	bullet.position = $Marker2D.global_position
+	
+	
+	move_and_slide()
