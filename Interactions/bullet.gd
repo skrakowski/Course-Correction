22b56db9c9710185cad21578a7cf40c2e1e101ba
rgipsonhansen <45026386+rgipsonhansen@users.noreply.github.com>
2023-11-21 13:32:57 -0700

    Basic collision on bullets

--- Interactions/bullet.gd
+++ Interactions/bullet.gd
@@ -2,9 +2,15 @@ extends CharacterBody2D
 
 var bVelocity = Vector2(1,0)
 var bSpeed = 300
+var time = 50
 
 func _physics_process(delta):
-	
 	move_and_collide(bVelocity.normalized() * delta * bSpeed)
+	time -= 1
+	if time < 0:
+		queue_free()
 
 
+func _on_area_2d_body_entered(body):
+	if body != self:
+		queue_free()
--- Player/player.gd
+++ Player/player.gd
@@ -3,17 +3,12 @@ extends CharacterBody2D
 const ACC = 5
 const bulletPath = preload("res://Interactions/bullet.tscn")
 
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
 
 func shoot():
 	var bullet = bulletPath.instantiate()
 
 	get_parent().add_child(bullet)
 	bullet.position = $Marker2D.global_position
-
-
 
 func _physics_process(_delta):
 
@@ -25,14 +20,7 @@ func _physics_process(_delta):
 	
 	velocity += direction * ACC
 	
-
-
-	
-#func shoot():
-#	var bullet = bulletPath.instantiate()
-#
-#	get_parent().add_child(bullet)
-#	bullet.position = $Marker2D.global_position
-	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 	
 	move_and_slide()
