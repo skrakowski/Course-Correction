31062da209e70ed63e8e3a1772a6e90e9df1b48c
skrakowski <bastiankraken@gmail.com>
2023-12-09 15:25:58 -0600

    Put weapons under a fire type var

--- Player/player.gd
+++ Player/player.gd
@@ -4,50 +4,49 @@ const ACC = 5
 const bulletPath = preload("res://Interactions/bullet.tscn")
 
 @onready var lasergun = $RayCast2D
-
+@export var fire_type = 1
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
-	if Input.is_action_just_pressed("triblast"):
-		triblast()
 
 #Creates the Triblast bullets
-func triblast():
-	var bullet = bulletPath.instantiate()
-	bullet.bVelocity = $Marker2D.global_position - global_position
-	bullet.global_position = $Marker2D.global_position
-	get_parent().add_child(bullet)
-	
-	var bullet2 = bulletPath.instantiate()
-	bullet2.bVelocity = $Marker2D2.global_position - global_position
-	bullet2.global_position = $Marker2D2.global_position
-	get_parent().add_child(bullet2)
-
-	var bullet3 = bulletPath.instantiate()
-	bullet3.bVelocity = $Marker2D3.global_position - global_position
-	bullet3.global_position = $Marker2D3.global_position
-	get_parent().add_child(bullet3)
-
-#Creates the Autocannon bullet
 func shoot():
-	var bullet = bulletPath.instantiate()
-	bullet.bVelocity = $Marker2D.global_position - global_position
-	bullet.global_position = $Marker2D.global_position
-	get_parent().add_child(bullet)
+	if fire_type == 1:
+		var bullet = bulletPath.instantiate()
+		bullet.bVelocity = $Marker2D.global_position - global_position
+		bullet.global_position = $Marker2D.global_position
+		get_parent().add_child(bullet)
+	if fire_type == 2:
+		var bullet = bulletPath.instantiate()
+		bullet.bVelocity = $Marker2D.global_position - global_position
+		bullet.global_position = $Marker2D.global_position
+		get_parent().add_child(bullet)
 	
+		var bullet2 = bulletPath.instantiate()
+		bullet2.bVelocity = $Marker2D2.global_position - global_position
+		bullet2.global_position = $Marker2D2.global_position
+		get_parent().add_child(bullet2)
+
+		var bullet3 = bulletPath.instantiate()
+		bullet3.bVelocity = $Marker2D3.global_position - global_position
+		bullet3.global_position = $Marker2D3.global_position
+		get_parent().add_child(bullet3)
+	if fire_type == 3:
+		if lasergun.is_colliding():
+			var target = lasergun.get_collider()
+			if target.is_in_group("enemy"):
+				target.health -= 1
+
+
+
 	
 
 func kill():
 	get_tree().reload_current_scene()
 
 #Laser Gun Functionality
-func _process(_delta):
-	if Input.is_action_just_pressed("laser"):
-		if lasergun.is_colliding():
-			var target = lasergun.get_collider()
-			if target.is_in_group("enemy"):
-				target.health -= 1
+
 				
 			
 
