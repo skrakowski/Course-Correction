89dacd96d70c02ceb3c97f888baaf99fb9caf7c8
skrakowski <bastiankraken@gmail.com>
2023-12-20 14:23:09 -0600

    Added stand-in enviorment

--- World/main_world.gd
+++ World/main_world.gd
@@ -1,7 +1,15 @@
 extends Node
 
+@onready var coll_ceil = $StaticBody2D/Collision_Ceil
+@onready var poly_ceil = $StaticBody2D/Collision_Ceil/Polygon_Ceil
+@onready var coll_floor = $StaticBody2D/Collision_Floor
+@onready var poly_floor = $StaticBody2D/Collision_Floor/Polygon_Floor
 @onready var pause_menu = $pauseMenu
 var paused = false
+
+func _ready():
+	poly_floor.polygon = coll_floor.polygon
+	poly_ceil.polygon = coll_ceil.polygon
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
